12554c8e4acfa3ad19b4ce94fc2e53f47238dae3
FakeDok <4o6@mail.ru>
2020-04-21 20:58:27 +0300

    the-end

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,7 @@ let homeTop = document.querySelectorAll('.home__content');
 console.log(homeBottom);
 console.log(homeTop);
 
-homeTop[0].style.display = 'block'; 
+homeTop[0].style.display = 'flex'; 
 
 for (let i = 0; i < homeBottom.length; i++) {
     homeBottom[i].addEventListener( 'click', function () {
@@ -16,26 +16,42 @@ for (let i = 0; i < homeBottom.length; i++) {
 function showCurrentContent(ind) {
     for (let b = 0; b < homeTop.length; b++){
         if (b === ind) {
-            homeTop[b].style.display = 'block';
+            homeTop[b].style.display = 'flex';
         } else {
             homeTop[b].style.display = 'none';
         }
     }
 }
 
-// let up = document.querySelectorAll('.home__content');
-// let bottom = document.querySelectorAll('.home__circle');
-// up.addEventListener('click', invisible);
-// function invisible() {
-//     bottom.classList.toggle("shadow");
-// }
-// console.log(up);
 
 const videoBtn = document.querySelector('.video__button');
-const video= document.querySelector('video');
+const video = document.querySelector('.video__movie');
 
 videoBtn.addEventListener('click', function () {
     video.play();
     console.log('playing');
     videoBtn.style.display = 'none'
 })
+
+let currentSlideState = 0;
+
+const rightArrow = document.querySelector ('.testimonial__right-arrow');
+const leftArrow = document.querySelector('.testimonial__left-arrow');
+const sliderContainer = document.querySelector('.testimonial__people');
+rightArrow.addEventListener('click', changeSliderToRight)
+leftArrow.addEventListener('click', changeSliderToLeft);
+
+function changeSliderToRight() {
+    if (currentSlideState > - 400){
+    currentSlideState = currentSlideState - 200;
+    sliderContainer.style.marginLeft = `${currentSlideState}px`;
+    }
+}
+
+function changeSliderToLeft() {
+    if (currentSlideState < 400 ){
+    currentSlideState = currentSlideState + 200;
+    sliderContainer.style.marginLeft = `${currentSlideState}px`;
+    }
+}
+
